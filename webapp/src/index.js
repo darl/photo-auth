9b20883feff9b2ed9f42b744c9910c36e9350401
Ivan Shirokov <shirokoff@yandex-team.ru>
2019-12-07 19:11:45 +0300

    WebApp up

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import VideoFrame from './VideoFrame';
+import VideoFrame from './VideoFrame/VideoFrame';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<VideoFrame />, document.getElementById('root'));
