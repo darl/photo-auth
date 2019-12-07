a7f4a696c40123551e766a638b9e6d63eb028e18
Ivan Shirokov <shirokoff@yandex-team.ru>
2019-12-07 16:36:38 +0300

    Webapp

---
+++ webapp/build/precache-manifest.1915589b8bac24b8c0c7ac9974b4e176.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f267d388aea6a8fb48345fb42d7cb2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cffcef7166f0a1de51e0",
+    "url": "/static/css/main.02f5f38e.chunk.css"
+  },
+  {
+    "revision": "86dc6cd6ce18cf22cd5b",
+    "url": "/static/js/2.4c676ac5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4c676ac5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cffcef7166f0a1de51e0",
+    "url": "/static/js/main.3f898fd0.chunk.js"
+  },
+  {
+    "revision": "ad905b8bef05c7c307cc",
+    "url": "/static/js/runtime-main.b80191ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
