0052fbb9828c67fa334311236606eaf987e80f63
aleien <aleien@yandex-team.ru>
2019-12-07 21:20:02 +0300

    Added simple onboarding slide

--- webapp/src/Onboarding/Onboarding.js
+++ webapp/src/Onboarding/Onboarding.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './Onboarding.css';
+import TouchCarousel from 'react-touch-carousel';
+import tutorial1 from './img/tutorial-1.png';
+import cross from './img/cross.png'
 
 class Onboarding extends React.PureComponent {
+
     render() {
         return (
-            <div className="Onboarding">
-                Put your slides here
+            <div>
+                <div className="Onboarding">
+                    <div className="Cross">
+                        <img src={cross} width={40}/>
+                    </div>
+                    <img className="Illustration"
+                        src={tutorial1}
+                        width={234}
+                    />
+
+                </div>
+                <div className="Emptyspace">
+                </div>
+                <div className="BlackText">
+                    <p>Теперь автокредит можно оформить онлайн!<br/><br/>Достаточно камеры смартфона</p>
+                </div>
             </div>
         );
     }
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import VideoFrame from './VideoFrame/VideoFrame';
 import * as serviceWorker from './serviceWorker';
+import Onboarding from "./Onboarding/Onboarding";
 
-ReactDOM.render(<VideoFrame />, document.getElementById('root'));
+ReactDOM.render(<Onboarding />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
