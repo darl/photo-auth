3f9cd5debdccc280e7c74f2456486ee64b6fcd7e
Ivan Shirokov <shirokoff@yandex-team.ru>
2019-12-07 19:18:49 +0300

    WebApp Onboarding

---
+++ webapp/src/Onboarding/Onboarding.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Onboarding.css';
+
+class Onboarding extends React.PureComponent {
+    render() {
+        return (
+            <div className="Onboarding">
+                Put your slides here
+            </div>
+        );
+    }
+}
+
+export default Onboarding;
