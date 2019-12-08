3176bd7d72ea1dbde55ac2d4fd3f947664087e47
Vladislav Dolbilov <darl@yandex-team.ru>
2019-12-08 15:30:19 +0300

    update static

--- webapp/build/service-worker.js
+++ webapp/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.499ee2fef0ee121ae0e0aaaf3d8d33d9.js"
+  "/precache-manifest.218489eed12c38414ae54090f22a524c.js"
 );
 
 self.addEventListener('message', (event) => {
